c7f3ff46101983e47c3e30656b00f375a2a70386
cfschulte <cfschulte@humonc.wisc.edu>
2019-03-07 15:43:54 -0600

    Setting the stage for next week.

--- index.php
+++ index.php
@@ -14,6 +14,7 @@
 
     <script type="text/javascript" src="/jquery/jquery.js"></script>
     <script type="text/javascript" src="/cecinestpasunewordpress/js/write.js"></script>
+    <script type="text/javascript" src="/cecinestpasunewordpress/js/playpen.js"></script>
 
 </HEAD>
 <body>
@@ -21,7 +22,7 @@
 
 <div class="landing">
     <h1>Ceci n'est pas une wordpress</h1>
-    <h2>really, it isn't. nor is it drupal</h2>
+    <h2>nor is it drupal</h2>
     <p>This is an experimental site to explore html5, css3, javascript, php, and maybe a bit of python and ruby. I sometimes talk about it in my <a href='rumination.html'>ruminations</a> and I try to use it in my <a href="playpen.php">Experiments</a>.</p>
     
    <div class="latest_ruminant">
